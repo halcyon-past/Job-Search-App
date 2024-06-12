e13640edf646e8f1285f3ab2155004e0101552fe
halcyon-past <titanssuperior@gmail.com>
2024-06-12 20:08:44 +0530

    First Commit

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View,Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+        <Text>Hello World This is Aritro, Welcome to React Native</Text>
+        </View>
+    );
+}
+
+export default Home;
